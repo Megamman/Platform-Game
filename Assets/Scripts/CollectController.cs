790a92d83b72d63275f863c9a27ba7fa6a24b00f
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-06-05 13:32:40 +0200

    coins update

---
+++ Assets/Scripts/CollectController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Collecting/Destroying Objects Script
+public class CollectController : MonoBehaviour {
+
+	void Start () {
+
+	}
+
+	// This function will trigger as soon as the player touches a ball.
+	// This function requires a parameter to be a collider
+	// Collider C is whatever the Capsule(player) will have touched.
+	void OnTriggerEnter(Collider c) {
+		
+		// If the Game Object attached to the collider is a ball, it gets destroyed.
+		if (c.gameObject.tag == "coin") {
+
+			Destroy (c.gameObject);
+
+			// This comand will add +1 to the Score Counter
+			PointsController.scoreValue += 1;
+
+			// this amkes the object destroyed
+			Destroy (c.gameObject);
+
+			// 	AudioSource source = GetComponent<AudioSource> ();
+			// 	source.Play ();
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/PointsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PointsController : MonoBehaviour {
+
+
+	public static int scoreValue = 0;
+
+	
+	Text score; 
+
+	public static int coinAmount;
+
+	// Use this for initialization
+	void Start () {
+
+		score = GetComponent <Text> ();
+
+	}
+	
+
+}

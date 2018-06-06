f6d9509efb0a566876ec4427952c080d5f8197fd
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-06-06 14:11:51 +0200

    Puzzles done

---
+++ Assets/Scripts/lever.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lever : MonoBehaviour {
+
+	Animator anim;
+	public GameObject target;
+	private PlayerMovement player;
+
+
+	void Awake()
+	{
+		anim = GetComponent<Animator> ();
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerMovement> ();
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.CompareTag ("Player")) {
+			if(tag == "firstLever"){
+				target.SetActive (false);
+				anim.SetTrigger ("pull");
+			}
+
+			if(tag == "SecondLever"){
+				target.SetActive (true);
+				anim.SetTrigger ("pull");
+			}
+
+
+
+
+		}
+
+
+	}
+
+}

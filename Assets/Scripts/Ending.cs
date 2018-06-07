7e57ae95874600fb1e1d25cf0a40a95a1799c22e
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-06-07 14:05:17 +0200

    Ending and some texture
    created a winnig screen
          this will apear when the player collides with an invisible collider next to the last tree
    add some more water texture and where the texture would stop bu the player could see

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour {
+
+	public GameObject target;
+	private PlayerMovement player;
+
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerMovement> ();
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.CompareTag ("Player")) {
+			target.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			target.SetActive (false);
+		}
+	}
+}
